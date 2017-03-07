3c848820d6bca03a234b784e9889cc1896bc0f28
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-06 20:47:02 -0800

    added react-tap-event-plugin

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+
 import { AppComponent } from './components/app';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
     <AppComponent />,
