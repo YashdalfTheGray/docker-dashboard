1b1952949470f4b2b7b097024242d8bd209a6134
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-06 20:45:22 -0800

    webpack should pull together everything frontend and create a bundle

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader']
+            }
+        ]
+    }
+}
