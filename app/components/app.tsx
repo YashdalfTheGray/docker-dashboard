81cafc9b6319dca735b464f1310437848c408fbe
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-07 19:50:24 -0800

    added the theme provider wrapper and an add button'

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
 
 export class AppComponent extends React.Component<{}, {}> {
     render() {
+        const newContainerButton = (
+            <IconButton
+                iconClassName="material-icons"
+                tooltip="New container"
+                style={{ marginRight: '8px' }}>
+                add
+            </IconButton>
+        );
+
         return (
-            <h1>Docker Dashboard v2</h1>
+            <MuiThemeProvider>
+                <AppBar
+                    title="Docker Dashboard"
+                    showMenuIconButton={false}
+                    iconElementRight={newContainerButton}/>
+            </MuiThemeProvider>
         );
     }
 }
