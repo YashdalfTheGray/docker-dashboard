e2ed6ecfe8444cd8f99e96e6d6adf204e3a8f468
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-09 20:59:02 -0800

    added some dummy data

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -2,8 +2,45 @@ import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import IconButton from 'material-ui/IconButton';
+import { partition } from 'lodash';
 
-export class AppComponent extends React.Component<{}, {}> {
+import ContainerListItem, { Container } from './ContainerListitem';
+import ContainerList from './ContainerList';
+
+class AppState {
+    containers?: Container[];
+    stoppedContainers?: Container[];
+}
+
+export class AppComponent extends React.Component<{}, AppState> {
+    containers: Container[] = [
+        {
+            id: '1',
+            name: 'Test Container',
+            image: 'httpd',
+            state: 'running',
+            status: 'Running'
+        },
+        {
+            id: '2',
+            name: 'Test Container 2',
+            image: 'node',
+            state: 'stopped',
+            status: 'Running'
+        }
+    ]
+
+    constructor() {
+        super();
+
+        const partitioned = partition(this.containers, c => c.state === 'running');
+
+        this.state = {
+            containers: partitioned[0],
+            stoppedContainers: partitioned[1]
+        };
+    }
+
     render() {
         const newContainerButton = (
             <IconButton
@@ -21,6 +58,8 @@ export class AppComponent extends React.Component<{}, {}> {
                         title="Docker Dashboard"
                         showMenuIconButton={false}
                         iconElementRight={newContainerButton}/>
+                    <ContainerList title="Running" containers={this.state.containers}></ContainerList>
+                    <ContainerList title="Stopped" containers={this.state.stoppedContainers}></ContainerList>
                 </div>
             </MuiThemeProvider>
         );
