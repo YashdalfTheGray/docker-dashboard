89c6f314fda169c5c6209e74bb0debb7340724d1
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-09 20:58:51 -0800

    added some margin to make things look nice

--- app/components/ContainerList.tsx
+++ app/components/ContainerList.tsx
@@ -10,10 +10,12 @@ export class ContainerListProps {
 export default class ContainerList extends React.Component<ContainerListProps, {}> {
     render() {
         return (
-            <div>
-                <h3>{this.props.title}</h3>
+            <div style={{ margin: '16px' }}>
+                <h3 style={{ fontFamily: 'Roboto' }}>{this.props.title}</h3>
                 <p>{ this.props.containers.length === 0 ? 'No containers to show' : '' }</p>
-                <div className="row">{this.props.containers.map(c => <ContainerListItem key={c.name} {...c} />)}</div>
+                <div style={{ margin: '8px' }}>
+                    {this.props.containers.map(c => <ContainerListItem key={c.name} {...c} />)}
+                </div>
             </div>
         );
     }
