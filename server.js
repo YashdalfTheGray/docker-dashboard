f6cf3fe21e117b33e9d950de4ff18ef9441f85ff
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-10 20:53:21 -0800

    wired up socket.io and the docker api

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const chalk = require('chalk');
+const socketIo = require('socket.io');
+const Server = require('http').Server;
+
+const docker = require('./dockerapi');
 
 const app = express();
+const server = Server(app);
+const io = socketIo(server);
 
 const port = process.env.PORT || process.argv[2] || 3000;
+
+function refreshContainers() {
+    docker.listContainers({ all: true }, (err, containers) => {
+        io.emit('containers.list', containers);
+    });
+}
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on ${chalk.green(port)}`);
+});
+
+io.on('connection', socket => {
+    socket.on('containers.list', () => {
+        refreshContainers();
+    });
 });
