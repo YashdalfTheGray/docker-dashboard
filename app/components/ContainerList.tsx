b6060dcee5c8d527165472d8163890639064fd64
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-30 13:54:43 -0800

    prettier containerList plus fixed props for containerlistitem

--- app/components/ContainerList.tsx
+++ app/components/ContainerList.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
 
-import ContainerListItem, { Container } from './ContainerListitem';
+import ContainerListItem, { Container } from './ContainerListItem';
 
 export class ContainerListProps {
-    containers: Container[];
-    title?: string;
+  containers: Container[];
+  title?: string;
 }
 
-export default class ContainerList extends React.Component<ContainerListProps, {}> {
-    render() {
-        return (
-            <div style={{ margin: '16px' }}>
-                <h3 style={{ fontFamily: 'Roboto' }}>{this.props.title}</h3>
-                <p style={{ fontFamily: 'Roboto' }}>{ this.props.containers.length === 0 ? 'No containers to show' : '' }</p>
-                <div style={{ margin: '8px' }}>
-                    {this.props.containers.map(c => <ContainerListItem key={c.name} {...c} />)}
-                </div>
-            </div>
-        );
-    }
+export default class ContainerList extends React.Component<
+  ContainerListProps,
+  {}
+> {
+  render() {
+    return (
+      <div style={{ margin: '16px' }}>
+        <h3 style={{ fontFamily: 'Roboto' }}>{this.props.title}</h3>
+        <p style={{ fontFamily: 'Roboto' }}>
+          {this.props.containers.length === 0 ? 'No containers to show' : ''}
+        </p>
+        <div style={{ margin: '8px' }}>
+          {this.props.containers.map(c => (
+            <ContainerListItem key={c.name} container={c} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
