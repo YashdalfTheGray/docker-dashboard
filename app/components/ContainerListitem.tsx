dc762f918d84331c813e72b739cd40198f0f3067
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-30 13:54:04 -0800

    fixed up container list so that it doesn't have errors

--- app/components/ContainerListitem.tsx
+++ app/components/ContainerListitem.tsx
@@ -1,4 +1,3 @@
-import * as io from 'socket.io-client';
 import * as React from 'react';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
@@ -6,54 +5,62 @@ import FlatButton from 'material-ui/FlatButton';
 import { getSocket } from '../services/socket';
 
 export interface Container {
-    id: string,
-    name: string,
-    image: string,
-    state: string,
-    status: string
+  id: string;
+  name: string;
+  image: string;
+  state: string;
+  status: string;
+}
+
+interface IContainerListItemProps {
+  container: Container;
 }
 
 const socket = getSocket();
 
-export default class ContainerListItem extends React.Component<Container, {}> {
-    constructor() {
-        super();
+export default class ContainerListItem extends React.Component<
+  IContainerListItemProps
+> {
+  public isRunning = () => {
+    const { container } = this.props;
+    return container.state === 'running';
+  };
 
-        this.handleStartStopClick = this.handleStartStopClick.bind(this);
-        this.handleDeleteClick = this.handleDeleteClick.bind(this);
-    }
-    isRunning() {
-        return this.props.state === 'running';
-    }
+  public handleStartStopClick = () => {
+    const { container } = this.props;
+    socket.emit(this.isRunning() ? 'container.stop' : 'container.start', {
+      id: container.id
+    });
+  };
 
-    handleStartStopClick() {
-        socket.emit(this.isRunning() ? 'container.stop' : 'container.start', { id: this.props.id });
-    }
+  public handleDeleteClick = () => {
+    const { container } = this.props;
+    socket.emit('container.remove', { id: container.id });
+  };
 
-    handleDeleteClick() {
-        socket.emit('container.remove', { id: this.props.id });
-    }
-
-    render() {
-        return (
-            <Card style={{ margin: '8px 0px' }}>
-                <CardTitle
-                    title={this.props.name}
-                    titleColor={ this.isRunning() ? '#00E676' : '#90A4AE'}/>
-                <CardText>
-                    <p>Name: {this.props.name}</p>
-                    <p>Image: <code>{this.props.image}</code></p>
-                    <p>Status: {this.props.status}</p>
-                </CardText>
-                <CardActions>
-                    <FlatButton
-                        label={ this.isRunning() ? 'Stop' : 'Start' }
-                        onClick={this.handleStartStopClick} />
-                    <FlatButton
-                        label="Remove"
-                        onClick={this.handleDeleteClick} />
-                </CardActions>
-            </Card>
-        );
-    }
+  render() {
+    const { container } = this.props;
+    return (
+      <Card style={{ margin: '8px 0px' }}>
+        <CardTitle
+          title={container.name}
+          titleColor={this.isRunning() ? '#00E676' : '#90A4AE'}
+        />
+        <CardText>
+          <p>Name: {container.name}</p>
+          <p>
+            Image: <code>{container.image}</code>
+          </p>
+          <p>Status: {container.status}</p>
+        </CardText>
+        <CardActions>
+          <FlatButton
+            label={this.isRunning() ? 'Stop' : 'Start'}
+            onClick={this.handleStartStopClick}
+          />
+          <FlatButton label="Remove" onClick={this.handleDeleteClick} />
+        </CardActions>
+      </Card>
+    );
+  }
 }
