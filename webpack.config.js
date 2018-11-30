890a49028bf4c5e5b77647aff5c52c218dfdb61c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-29 22:06:23 -0800

    updated the webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
+const { resolve } = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-    entry: './app/index.tsx',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/public'
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ['babel-loader', 'ts-loader']
-            }
-        ]
-    }
-}
+  entry: ['@babel/polyfill', './app/index.tsx'],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, './public')
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader']
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin(['public/*.js', 'public/*.js.map'])],
+  stats: {
+    colors: true
+  }
+};
