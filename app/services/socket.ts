3eedadd85116dc293fe6d81b4a0b497acbb5b9de
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-12-03 18:58:05 -0800

    gave the socket create a default

--- app/services/socket.ts
+++ app/services/socket.ts
@@ -2,13 +2,11 @@ import * as io from 'socket.io-client';
 
 let socket: typeof io.Socket;
 
-function getSocket(url?: string) {
-    if (!socket) {
-        socket = io.connect(url);
-    }
-    return socket;
+function getSocket(url: string = window.location.href) {
+  if (!socket) {
+    socket = io.connect(url);
+  }
+  return socket;
 }
 
-export {
-    getSocket
-};
+export { getSocket };
