00ed08ff84593cc40f1f7b7adf3c6e2164a7f189
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-12-24 12:36:47 -0500

    added socket events to make event listening and subscribing easier

---
+++ server/socket-events.ts
@@ -0,0 +1,24 @@
+export const listContainers = 'containers.list';
+export const listContainersAck = 'containers.list.ack';
+export const listContainersSuccess = 'containers.list.success';
+export const listContainersError = 'containers.list.error';
+
+export const startContainer = 'container.start';
+export const startContainerAck = 'container.start.ack';
+export const startContainerSuccess = 'container.start.success';
+export const startContainerError = 'container.start.error';
+
+export const stopContainer = 'container.stop';
+export const stopContainerAck = 'container.stop.ack';
+export const stopContainerSuccess = 'container.stop.success';
+export const stopContainerError = 'container.stop.error';
+
+export const removeContainer = 'container.remove';
+export const removeContainerAck = 'container.remove.ack';
+export const removeContainerSuccess = 'container.remove.success';
+export const removeContainerError = 'container.remove.error';
+
+export const newContainer = 'container.new';
+export const newContainerAck = 'container.new.ack';
+export const newContainerSuccess = 'container.new.success';
+export const newContainerError = 'container.new.error';
