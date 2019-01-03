753ca25b5fab31e1758492b1592c29092968835b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-01-02 19:31:58 -0800

    added hmr to the client code

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppComponent from './components/app';
 
-ReactDOM.render(
+const WrappedApp = hot(() => (
   <CssBaseline>
     <AppComponent />
-  </CssBaseline>,
-  document.querySelector('#app')
-);
+  </CssBaseline>
+));
+
+ReactDOM.render(<WrappedApp />, document.querySelector('#app'));
