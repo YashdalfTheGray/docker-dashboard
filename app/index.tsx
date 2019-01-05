c624e56975ed637bfd54cc6af79bafddcccaef67
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-01-04 18:48:50 -0800

    remove the react-hot-loader from the same file as render

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader/root';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppComponent from './components/app';
 
-const WrappedApp = hot(() => (
+ReactDOM.render(
   <CssBaseline>
     <AppComponent />
-  </CssBaseline>
-));
-
-ReactDOM.render(<WrappedApp />, document.querySelector('#app'));
+  </CssBaseline>,
+  document.querySelector('#app')
+);
