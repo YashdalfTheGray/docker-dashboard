7b694685668a35cb94ae957312362b53f7740afb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-12-18 01:13:13 -0800

    added a simple logs dialog to the components

--- app/components/LogsDialog.tsx
+++ app/components/LogsDialog.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import createStyles from '@material-ui/core/styles/createStyles';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
+
+import { IContainer } from '../types/Container';
 
 const logsDialogStyles = (theme: Theme) =>
   createStyles({
@@ -15,25 +20,38 @@ const logsDialogStyles = (theme: Theme) =>
   });
 
 interface ILogsDialogProps {
+  container: IContainer;
   onClose: () => void;
   isOpen: boolean;
 }
 
-type LogsDialogProps = WithStyles<typeof logsDialogStyles> & ILogsDialogProps;
+type LogsDialogProps = ILogsDialogProps & WithStyles<typeof logsDialogStyles>;
 
 class LogsDialog extends React.Component<LogsDialogProps> {
   public render() {
-    const { onClose, isOpen } = this.props;
+    const { container, onClose, isOpen } = this.props;
 
     return (
       <Dialog
         onClose={onClose}
         aria-labelledby="simple-dialog-title"
         open={isOpen}>
-        <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+        <DialogTitle id="simple-dialog-title">
+          Logs for {container.name}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {['some code here', 'some more code here'].map((l, i) => (
+              <pre key={`logline-${i}`}>{l}</pre>
+            ))}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Close</Button>
+        </DialogActions>
       </Dialog>
     );
   }
 }
 
-export default withStyles;
+export default withStyles(logsDialogStyles)(LogsDialog);
