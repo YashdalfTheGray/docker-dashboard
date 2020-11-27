95a624e837eb2ae89955cad429cb981d644310b2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-27 03:34:10 -0500

    remove debug when we are in prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,27 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+const isDev = (mode) => mode === 'development';
+const isProd = (mode) => mode === 'production';
+
+module.exports = (_, argv) => ({
   entry: [
     'core-js/stable',
     'regenerator-runtime/runtime',
     'webpack-hot-middleware/client',
-    './app/index.tsx'
+    './app/index.tsx',
   ],
   output: {
     filename: 'bundle.js',
     path: resolve(__dirname, './public'),
     publicPath: '/',
     hotUpdateChunkFilename: '.hot/[id].[contenthash].hot-update.js',
-    hotUpdateMainFilename: '.hot/[contenthash].hot-update.json'
+    hotUpdateMainFilename: '.hot/[contenthash].hot-update.json',
   },
   devtool: 'source-map',
+  mode: argv.mode,
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss']
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss'],
   },
   module: {
     rules: [
@@ -29,20 +33,20 @@ module.exports = {
           { loader: 'babel-loader' },
           {
             loader: 'ts-loader',
-            options: { configFile: 'tsconfig.client.json' }
-          }
-        ]
-      }
-    ]
+            options: { configFile: 'tsconfig.client.json' },
+          },
+        ].concat(isProd(argv.mode) ? [{ loader: 'webpack-remove-debug' }] : []),
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin({
       verbose: true,
-      cleanOnceBeforeBuildPatterns: ['public/*.js', 'public/*.js.map']
+      cleanOnceBeforeBuildPatterns: ['public/*.js', 'public/*.js.map'],
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   stats: {
-    colors: true
-  }
-};
+    colors: true,
+  },
+});
