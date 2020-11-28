c2820bfec997cd30f4adea9f8307aa81e5d5a1cf
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-28 04:58:21 -0500

    added some documentation as well as flowed the types better

--- server/hmr.ts
+++ server/hmr.ts
@@ -13,6 +13,14 @@ type WebpackConfigAndCompiler = {
   compiler: webpack.Compiler;
 };
 
+/**
+ * hotModuleReloadingSetup sets up the correct HMR middleware for
+ * an express application. This supports both a functional webpack config
+ * as well as a static webpack config.
+ * @param app the express application to set up HMR for
+ * @param configFilePath the webpack configuration file path
+ * @returns the fully resolved webpack configuration as well as the compiler
+ */
 export default function hotModuleReloadingSetup(
   app: express.Application,
   configFilePath: string = '../../webpack.config.js'
@@ -31,11 +39,22 @@ export default function hotModuleReloadingSetup(
   return { config, compiler };
 }
 
-function getWebpackConfig<F extends WebpackConfigFunction>(
+/**
+ * getWebpackConfig supports extracting webpack configuration given a path
+ * whether it resolves to a static configuration or a function that returns
+ * a webpack configuration.
+ *
+ * By default, this function (in terms of typings), is expecting a function
+ * that resolves into a config after passing in `args`. To type it properly for
+ * a static config, use `getWebpackConfig<webpack.Configuration>(filePath);`
+ * @param path the path of the config file to load
+ * @param args the args to pass to the function if the config is in function form
+ */
+function getWebpackConfig<F = WebpackConfigFunction>(
   path: string,
-  ...args: Parameters<F>
+  ...args: F extends WebpackConfigFunction ? Parameters<F> : never
 ): webpack.Configuration {
-  const configOrFunction: F | webpack.Configuration = require(path);
+  const configOrFunction: F = require(path);
 
   if (typeof configOrFunction === 'function') {
     return configOrFunction.call(null, ...args);
